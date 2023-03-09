6e1918c6cda9fcc8aa9f1b22dd1f1a18270937e6
Ivoine Strachan <70272280+ivoinestrachan@users.noreply.github.com>
2023-03-08 18:35:36 -0800

    Add files via upload

---
+++ calculator/calculator/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  calculator
+//
+//  Created by Ivoine Strachan on 08/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
